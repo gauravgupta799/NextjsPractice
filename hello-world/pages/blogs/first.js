9a7a1778d1b0d1fff50f0ba1d1fd4f2e3a0bfd35
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-13 15:37:02 +0530

    nested-routes

---
+++ hello-world/pages/blogs/first.js
@@ -0,0 +1,9 @@
+const First = () => {
+  return (
+    <div>
+        <h2>First Blog</h2>
+    </div>
+  )
+}
+
+export default First
---
+++ hello-world/pages/blogs/second.js
@@ -0,0 +1,9 @@
+const Second = () => {
+  return (
+    <div>
+        <h2>Second Blog</h2>
+    </div>
+  )
+}
+
+export default Second
