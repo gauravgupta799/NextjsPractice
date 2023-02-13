76fd47550ede0fcb6ddae82f828b739e229e688c
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-13 18:28:46 +0530

    custom-404-page

---
+++ hello-world/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div style={{textAlign:'center'}}>
+        <h1>404 Page Not Found</h1>
+        <p>Custome 404 Page</p>
+    </div>
+  )
+}
+
+export default PageNotFound
