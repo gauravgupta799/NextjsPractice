f838498618443621e9206c08d427f9481ac69127
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-13 15:28:09 +0530

    page-based-routing

--- hello-world/pages/index.js
+++ hello-world/pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+// import { Inter } from '@next/font/google'
+// import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,8 +13,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h2>Hello world</h2>
+      <main>
+        <h2>This is Homepage</h2>
       </main>
     </>
   )
