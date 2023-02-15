f07eb5291e8c6f9ab6c9d8a9b5083a5396d15feb
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-15 13:24:07 +0530

    getStaticPaths

--- pre-rendering/pages/posts/[postId].js
+++ pre-rendering/pages/posts/[postId].js
@@ -13,6 +13,22 @@ function Post({post}){
 
 export default Post;
 
+export async function getStaticPaths() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await response.json();
+    const paths = data.map((post) => {
+        return {
+            params:{
+                postId:`${post.id}`
+            }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+
 export async function getStaticProps(context){
     const {params} = context
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
