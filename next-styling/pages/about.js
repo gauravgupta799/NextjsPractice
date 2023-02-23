472151d49cbf2f5ffa3e230591ac3804a5f4203c
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-23 18:18:47 +0530

    styled-with-SASS

--- next-styling/pages/about.js
+++ next-styling/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from "../styles/About.module.css";
+import scssStyles from "../styles/About.module.scss";
 
 const About = () => {
   return (
     <div>
         <h1 className={styles.title}>This is About page</h1>
+        <h1 className={scssStyles.titleScss}>Styled by using SASS</h1>
     </div>
   )
 }
--- next-styling/pages/contact.js
+++ next-styling/pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from "../styles/Contact.module.css";
+import scssStyles from "../styles/Contact.module.scss";
 
 const Contact = () => {
   return (
     <div>
         <h1 className={styles.title}>This is Contact page.</h1>
+        <h1 className={scssStyles.titleScss}>Styled by using SASS</h1>
     </div>
   )
 }
