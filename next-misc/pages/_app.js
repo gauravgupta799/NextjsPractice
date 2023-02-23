db5195f08992a6366796e00277e7bfb8bd722ab9
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-23 20:02:52 +0530

    Head Component

--- next-misc/pages/_app.js
+++ next-misc/pages/_app.js
@@ -1,5 +1,6 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import Head from 'next/head'
 import '@/styles/globals.css'
 import '../styles/layout.css'
 
@@ -12,6 +13,12 @@ export default function App({ Component, pageProps }) {
  
   return (
     <>
+      <Head>
+        <title>Codeverse</title>
+        <meta name="description" content="Awesome Youtube channel" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header/>
       <Component {...pageProps} />
       <Footer/>
