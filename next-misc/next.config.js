c502cdae0f57e59df24e6dc449182003796657dc
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-24 19:05:20 +0530

    redirects

--- next-misc/next.config.js
+++ next-misc/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects:[
+    {
+      source:'/about',
+      destination:'/',
+      permanent:true,
+    }
+  ]
 }
 
 module.exports = nextConfig
