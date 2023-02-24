ef1a5482184a4c289451a0df9115d404111bc625
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-24 11:33:56 +0530

    image-component

---
+++ next-misc/pages/pets.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from 'next/image';
+import img from "../public/1.jpg";
+
+const Pets = () => {
+  return (
+    <div>
+        <h2>Pets Images</h2>
+        <Image 
+          src={img} 
+          placeholder='blur' 
+          alt="pet"
+          width="280"
+          height="350"
+          />
+        {
+            ['1','2','3','4','5'].map((path)=>{
+                return(
+                    <div key={path} style={{
+                        display:"inline-block",
+                        margin:"5px"
+                        }}>
+                        <Image 
+                        src={`/${path}.jpg`}
+                        alt="pet"
+                        width="280"
+                        height="350"
+                        />
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default Pets
