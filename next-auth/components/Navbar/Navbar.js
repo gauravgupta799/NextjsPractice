ee42c7533de6de18271897a4325c26252170cfbb
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-02-27 15:36:45 +0530

    client-side authentication

--- next-auth/components/Navbar/Navbar.js
+++ next-auth/components/Navbar/Navbar.js
@@ -1,45 +1,71 @@
 import React from 'react';
 import Link from "next/link";
-import {signIn, signOut} from "next-auth/react";
+import {signIn, signOut, useSession} from "next-auth/react";
+import Image from "next/image";
 
 const Navbar = () => {
+  const {data:session, status } = useSession()
+  console.log(session, status)
+
   return (
     <nav className="header">
-    <h1 className="logo">
-        <a href="/">NextAuth</a>
-    </h1>
-    <ul className="main-nav">
+      <h1 className="logo">
+          <a href="/">NextAuth</a>
+      </h1>
+      <ul className="main-nav">
         <li>
             <Link href="/">
               Home
             </Link>
         </li>
-        <li>
-            <Link href="/dashboard">
-                 Dashboard
-            </Link>
-        </li>
-        <li>
-            <Link href="/blog">
-              Blog
-            </Link>
-        </li>
-        <li>
-            <Link href="/api/auth/signin">
-              <span onClick={()=>{
-                e.preventDefault()
-                signIn('github')
-              }}>Signin</span>
-            </Link>
-        </li>
-        <li>
-            <Link href="/api/auth/signout">
-              <span onClick={()=>{
-                e.preventDefault()
-                signOut()
-              }}>Sign Out</span>
-            </Link>
-        </li>
+      { status === "authenticated" && (
+        <>
+            <li>
+                <Link href="/dashboard">
+                    Dashboard
+                </Link>
+            </li>
+            <li>
+                <Link href="/blog">
+                  Blog 
+                </Link>
+            </li>
+            <li>
+                 <span>{session.user.name}</span>
+            </li>
+            <li>
+                 <img src={session.user.image} alt="profileImg" style={{width:"30px", height:"30px",borderRadius:"50%"}} />
+            </li>
+            <li>
+                <Link href="/api/auth/signout">
+                  <span onClick={()=>{
+                    e.preventDefault()
+                    signOut()
+                  }}>Sign Out</span>
+                </Link>
+            </li>
+        </>
+      )}
+        {status === "unauthenticated" && (
+            <li>
+                <Link href="/api/auth/signin">
+                  <span onClick={()=>{
+                    e.preventDefault()
+                    signIn('github')
+                  }}>Signin</span>
+                </Link>
+            </li>
+        )}
+        {/* {status === "authenticated" && (
+            <li>
+                <Link href="/api/auth/signout">
+                  <span onClick={()=>{
+                    e.preventDefault()
+                    signOut()
+                  }}>Sign Out</span>
+                </Link>
+            </li>
+        )} */}
     </ul>
 
     </nav>
--- next-auth/pages/_app.js
+++ next-auth/pages/_app.js
@@ -1,13 +1,16 @@
 import Navbar from '@/components/Navbar/Navbar.js'
 import '@/styles/globals.css'
 import "../components/Navbar/Navbar.css";
+import {  SessionProvider } from 'next-auth/react';
 
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <SessionProvider session={pageProps.session}>
       <Navbar/>
-      <Component {...pageProps} />
+      <Component {...pageProps.session} />
+    </SessionProvider>
     </>
   )
 }
